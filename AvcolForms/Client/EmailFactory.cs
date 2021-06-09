16b8b0dc5f8251a77f845a0fe0ce7e1972670247
DJ Pownall <ac107151@avcol.school.nz>
2021-06-10 11:57:19 +1200

    Basic HTML email formatting with image attachments

--- AvcolForms/Client/EmailFactory.cs
+++ AvcolForms/Client/EmailFactory.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -13,21 +14,25 @@ namespace AvcolForms.Client
     public class EmailFactory
     {
 
-        public static void SendToDepartment(string address, Form form, byte[] attachment) // Temporary function for testing purposes
+        private static string GetHTMLStyle()
+        {
+            return "<style>\r\ntable, th, td {\r\n  border: 1px solid black;\r\n  border-collapse: collapse;\r\n}\r\nth, td {\r\n  padding: 4px;\r\n  text-align: left;    \r\n}\r\n</style>";
+        }
+        public static void SendToDepartment(string address, FormData form, MemoryStream attachment) // Temporary function for testing purposes
         {
             string[] dep_array = { address };
             SendToDepartments(dep_array, form, attachment);
         }
-        public static void SendToDepartments(string[] addresses, Form form, byte[] attachment)
+        public static void SendToDepartments(string[] addresses, FormData form, MemoryStream attachment)
         {
 
             // User credentials
             string from_address = "";
-            string authpassword = ""; // This obviously won't be defined in plain text, .NET is far too easy to decompile
+            string authpassword = Encoding.UTF8.GetString(Convert.FromBase64String("")); // This obviously won't be defined in plain text, .NET is far too easy to decompile
 
             // Define SMTP Server parameters
             // These are placeholders, currently waiting on approval from the IT department
-            SmtpClient smtpClient = new SmtpClient("smtp.avcol.school.nz", 123);
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
             smtpClient.EnableSsl = true;
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtpClient.UseDefaultCredentials = false;
@@ -38,13 +43,25 @@ namespace AvcolForms.Client
             foreach (string to_usr in addresses) message.To.Add(to_usr);
             message.From = new MailAddress(from_address);
             message.Subject = "Form Submission - " + form.Name;
-            message.Body = "A member has submitted a digital copy of the '" + form.Name + "' Form. Please see the attachment for their submission";
 
-            MemoryStream fstream = new MemoryStream(attachment);
-            string filename = from_address.Replace("@avcol.school.nz", "") + "_" + form.Name + ".txt"; // Use dynamic form names, file extension may change later
-            message.Attachments.Add(new Attachment(fstream, filename));
+
+            // Image attachments, might want to give it its own method sometime later
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Attachment headimage = new Attachment(assembly.GetManifestResourceStream("AvcolForms.Resources.avcolforms_logo.png"), "headimg.png");
+            headimage.ContentId = "logohead";
+            message.Attachments.Add(headimage);
+
+            // HTML Formatting
+            message.IsBodyHtml = true;
+            message.Body = GetHTMLStyle();
+            message.Body += "<img width=\"128\" height=\"128\" src=\"cid:logohead\"/><h1>Avcol Forms</h1>\n<p>A user has submitted a digital copy of the '" + form.Name + "' Form. Please see the attachment for their submission.</p>\n\n";
+            message.Body += DataExporter.GenerateHTMLTable(form);
+
+            //string filename = form.Name + from_address.Replace("@avcol.school.nz", "_") + ".xlsx"; // Use dynamic form names, file extension may change later
+
 
             smtpClient.Send(message);
+            //attachment.Close();
         }
     }
 }
